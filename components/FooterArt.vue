e202a88d325a86a6e61d25b2cfa066ef64c09ef3
Xanniiii <asa.annisa0105@gmail.com>
2023-09-03 18:06:38 +0700

    percobaan11

---
+++ components/FooterArt.vue
@@ -0,0 +1,30 @@
+<template>
+    <footer class="footer">
+      <div class="footer-content">
+        <p>&copy; 2023 Art Mosaic. All rights reserved.</p>
+      </div>
+    </footer>
+</template>
+  
+  <style>
+  .footer {
+    background-color: #f5f5f5;
+    padding: 20px 0;
+  }
+  
+  .footer-content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 0 20px;
+  }
+  
+  .footer p {
+    color: #666666;
+    font-size: 14px;
+    margin: 0;
+  }
+  
+  </style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Welcome to Our Website</h1>
+    <p>Art Mosaic adalah wadah kreatif yang memadukan beragam bakat dan visi seniman dari seluruh dunia. Seperti membangun sebuah mozaik yang indah dari potongan-potongan kecil, platform ini berfungsi sebagai pusat kolaborasi untuk seniman multidisiplin. Setiap seniman adalah potongan penting dari gambar besar yang menjadi karya seni kolektif yang menakjubkan.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+};
+</script>
